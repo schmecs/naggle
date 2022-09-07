941147b1a889e60053f42d63546787847afeb2ba
schmecs <schmecs@gmail.com>
2022-09-07 06:50:30 -0400

    Remove unused class

--- app/src/main/java/com/rebeccablum/naggle/notif/NagNotificationService.kt
+++ app/src/main/java/com/rebeccablum/naggle/notif/NagNotificationService.kt
@@ -15,31 +15,6 @@ import timber.log.Timber
 
 const val JOB_NEXT_NOTIFICATION = "next_notification"
 
-class NagNotificationService : JobIntentService() {
-
-    private val nagRepository: NagRepository by inject()
-    private val coroutineScope = CoroutineScope(Job())
-
-    companion object {
-        fun enqueueWork(context: Context, intent: Intent) {
-            //enqueueWork(context, NagNotificationService::class.java, JOB_NEXT_NOTIFICATION, intent)
-        }
-    }
-
-    override fun onHandleWork(intent: Intent) {
-        val nagId = intent.getIntExtra(NAG_ID, -1)
-        if (intent.action == ACTION_DISMISS) {
-            coroutineScope.launch {
-                nagRepository.markNagDismissed(nagId)
-            }
-        } else if (intent.action == ACTION_MARK_COMPLETE) {
-            coroutineScope.launch {
-                nagRepository.markNagCompleted(nagId)
-            }
-        }
-    }
-}
-
 // TODO update Koin and use CoroutineWorker
 class NagNotificationWorker(context: Context, workerParameters: WorkerParameters) :
     Worker(context, workerParameters), KoinComponent {
