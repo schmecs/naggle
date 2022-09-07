5543f6b7b3a4ceb42012e1e56d231c8ca7701897
schmecs <schmecs@gmail.com>
2022-09-07 06:47:37 -0400

    Replace JobIntentService with WorkManager

--- app/src/main/java/com/rebeccablum/naggle/notif/NagNotificationService.kt
+++ app/src/main/java/com/rebeccablum/naggle/notif/NagNotificationService.kt
@@ -3,13 +3,17 @@ package com.rebeccablum.naggle.notif
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.JobIntentService
+import androidx.work.*
 import com.rebeccablum.naggle.repo.NagRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
+import org.koin.core.KoinComponent
+import org.koin.java.KoinJavaComponent.inject
+import timber.log.Timber
 
-private const val JOB_NEXT_NOTIFICATION = 1000
+const val JOB_NEXT_NOTIFICATION = "next_notification"
 
 class NagNotificationService : JobIntentService() {
 
@@ -18,7 +22,7 @@ class NagNotificationService : JobIntentService() {
 
     companion object {
         fun enqueueWork(context: Context, intent: Intent) {
-            enqueueWork(context, NagNotificationService::class.java, JOB_NEXT_NOTIFICATION, intent)
+            //enqueueWork(context, NagNotificationService::class.java, JOB_NEXT_NOTIFICATION, intent)
         }
     }
 
@@ -33,5 +37,43 @@ class NagNotificationService : JobIntentService() {
                 nagRepository.markNagCompleted(nagId)
             }
         }
+    }
+}
+
+// TODO update Koin and use CoroutineWorker
+class NagNotificationWorker(context: Context, workerParameters: WorkerParameters) :
+    Worker(context, workerParameters), KoinComponent {
+
+    private val nagRepository: NagRepository by inject(NagRepository::class.java)
+    private val coroutineScope = CoroutineScope(Job())
+
+    companion object {
+        private const val ACTION = "action"
+        fun buildWorkRequest(action: String, nagId: Int): OneTimeWorkRequest {
+            val data = Data.Builder().putString(ACTION, action).putInt(NAG_ID, nagId).build()
+            return OneTimeWorkRequestBuilder<NagNotificationWorker>().apply { setInputData(data) }
+                .build()
+        }
+    }
+
+    override fun doWork(): Result {
+        val action: String? = inputData.getString(ACTION)
+        val nagId: Int = inputData.getInt(NAG_ID, -1)
+        if (action != null && nagId != -1) {
+            if (action == ACTION_DISMISS) {
+                coroutineScope.launch {
+                    nagRepository.markNagDismissed(nagId)
+                    Timber.d("Dismissed ID $nagId")
+                }
+            } else if (action == ACTION_MARK_COMPLETE) {
+                coroutineScope.launch {
+                    nagRepository.markNagCompleted(nagId)
+                    Timber.d("Marked complete ID $nagId")
+                }
+            }
+            return Result.success()
+        }
+        Timber.d("Missing input data: action $action, nagId $nagId")
+        return Result.failure()
     }
 }
--- app/src/main/java/com/rebeccablum/naggle/notif/UpdateNotificationsReceiver.kt
+++ app/src/main/java/com/rebeccablum/naggle/notif/UpdateNotificationsReceiver.kt
@@ -3,6 +3,8 @@ package com.rebeccablum.naggle.notif
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import timber.log.Timber
@@ -20,7 +22,16 @@ class UpdateNotificationsReceiver : BroadcastReceiver(), KoinComponent {
                     nagNotificationManager.cancelNotification()
                 }
                 if (intent.action in listOf(ACTION_DISMISS, ACTION_MARK_COMPLETE)) {
-                    NagNotificationService.enqueueWork(context, intent)
+                    val workManager = WorkManager.getInstance(context)
+                    val workRequest = NagNotificationWorker.buildWorkRequest(
+                        intent.action!!,
+                        intent.getIntExtra(NAG_ID, -1)
+                    )
+                    workManager.enqueueUniqueWork(
+                        JOB_NEXT_NOTIFICATION,
+                        ExistingWorkPolicy.KEEP,
+                        workRequest
+                    )
                 }
             }
         }
